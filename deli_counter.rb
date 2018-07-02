83e8e15811dd91ac67805f14c12b5c47dc50f29a
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-02 21:52:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 def line(array)
   if array.empty?
-    return true
+    return "The line is currently empty."
   else
     line_name_n_numbers = []
     array.each { |person|
-    line_name_n_numbers << "#{array.index(person)}. #{person}" }
-    "The line is currently #{line_name_n_numbers.join(' ')}"
+    line_name_n_numbers.push("#{array.index(person)}. #{person}") }
+    return "The line is currently #{line_name_n_numbers.join(' ')}"
   end
 end
