db3b45a326df6ec5aa7308cfc3a3320b500bbb62
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-02 21:53:28 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(array)
   else
     line_name_n_numbers = []
     array.each { |person|
-    line_name_n_numbers.push("#{array.index(person)}. #{person}") }
+    line_name_n_numbers.push("#{array.index(person) + 1}. #{person}") }
     puts "The line is currently #{line_name_n_numbers.join(' ')}"
   end
 end
