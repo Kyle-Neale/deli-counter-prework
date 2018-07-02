3635bac122d42b5e2aef407b5005b176278f5b0c
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-02 21:53:05 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ def line(array)
     line_name_n_numbers = []
     array.each { |person|
     line_name_n_numbers.push("#{array.index(person)}. #{person}") }
-    return "The line is currently #{line_name_n_numbers.join(' ')}"
+    puts "The line is currently #{line_name_n_numbers.join(' ')}"
   end
 end
