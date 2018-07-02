4598b900e23854d88fd7b5f31270a1af976e4f40
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-02 22:04:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,6 +10,6 @@ def line(array)
 end
 
 def take_a_number(katz_deli, name)
-  katz_deli << name
+  katz_deli.push(name)
   puts "Welcome #{name}. You are number #{katz_deli.index(name) + 1} in line."
 end
