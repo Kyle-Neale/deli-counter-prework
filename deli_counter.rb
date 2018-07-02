520f1a871b453c3b5f94ed2424c1ab8c8ac20ffd
Kyle Neale <kyle.a.neale@gmail.com>
2018-07-02 21:40:38 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,4 +2,6 @@ def line(array)
   if array.empty?
     return true
   else
-    
+    line_name_n_numbers = []
+    array.each { |person|
+    line_name_n_numbers << "#{array.index(person)}. #{}person
